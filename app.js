e8394e73038cfdd5e6140b63d36c8e99802eca3b
AltafBhai90 <henrybhai00@gmail.com>
2023-10-25 19:51:55 +0500

    initial commit for uploading

---
+++ app.js
@@ -0,0 +1,73 @@
+// var a = document.getElementsByTagName(('img')[2])
+// var a = document.getElementById('image_logo')
+// var a = document.getElementsByClassName('paras')
+
+// var a = document.createElement('img')
+// a.setAttribute('src','saifi.jpg')
+// console.log(a);
+
+// var parentNode = document.getElementById('node')
+// parentNode.innerHTML += '<p> jdksjdksdjw </p>'
+// parentNode.innerHTML += '<p> art</p>'
+// document.write(parentNode)
+
+// `Template Literls`
+
+// var count = 0;
+// function recursive(){
+//     count = count + 1
+//     if(count === 10){
+//         return
+//     }
+//     console.log(count);
+//     recursive()
+// }
+// recursive()
+
+// setTimeout(()=>{
+//     console.log("Executed");
+// },1000)
+// setTimeout(call Back, delay)
+
+// setInterval(()=>{
+//     console.log("Executed");
+// },1000)
+
+// var secNode = document.getElementById('sec')
+// var flag = false
+// var seconds = 0
+
+// function stopWatchCalculate(){
+//     console.log("Flag",flag);
+//     if(flag){
+//         seconds = seconds + 1
+//         secNode.innerText= seconds
+//         // setTimeout(stopWatchCalculate(),1000)
+        
+//     }
+// }
+// function start(){
+//     flag = true
+//     // stopWatchCalculate()
+// }
+// function stop(){
+//     flag = false
+//     // stopWatchCalculate()
+// }
+// function reset(){
+   
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
